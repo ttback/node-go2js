20243bd5dcbf71358039ba4ad5346696cab79287
Dev <ttback@gmail.com>
2014-05-22 07:57:07 -0400

    Remove all the sync function calls and use tmp file for go file

--- go.js
+++ go.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
+require('longjohn');
+
 var program = require('commander');
 var fs = require('fs');
 var esprima = require('esprima');
 var exec = require('exec');
 var path = require('path');
 var mv = require('mv');
+var tmp = require('tmp');
+tmp.setGracefulCleanup();
 
 //Config flags
-var TMP_GO_FILE_PATH = "./tmp.go";
 var INPUT_source_file_path = undefined;
 var JS_AST_OUTPUT_ENABLED = false;
 var DEFAULT_JS_OUTPUT_DIR_PATH = "./";
@@ -23,62 +26,46 @@ function outputFile(path) {
 }
 
 function parse(code, callback) {
-    fs.writeFile(TMP_GO_FILE_PATH, code, function(err) {
-        if (err) {
-            throw err;
+    _parseCode(code, function(ast) {
+        if (!ast) {
+            callback(new Error("No ast tree was generated"));
+        } else {
+            callback(ast);
         }
-
-        parseFile(TMP_GO_FILE_PATH, function(ast){
-            if (!ast){
-                throw new Error("No ast tree was generated");
-            }else{
-                callback(ast);
-            }
-        });
     });
 }
 
 function wrap(code, imports, callback) {
     var wrap_stmts = [];
     wrap_stmts.push("package main");
-    
-    if (typeof(imports) == "function"){
+
+    if (typeof(imports) == "function") {
         callback = imports;
         wrap_stmts.push('import(' + '"fmt"' + ')');
     }
-    
-    if (imports instanceof Array){
-        var import_strs=imports.length>1 ? '\n"'+ imports.join('"\n"') + '"' : '"'+imports[0] +'"'
+
+    if (imports instanceof Array) {
+        var import_strs = imports.length > 1 ? '\n"' + imports.join('"\n"') + '"' : '"' + imports[0] + '"'
         wrap_stmts.push('import(' + import_strs + ')');
     }
 
     var wrap_prefix = wrap_stmts.join("\n");
-    var wrapped_code = wrap_prefix +  '\nfunc main(){\n' + code + '}';
-    
-    fs.writeFile(TMP_GO_FILE_PATH, wrapped_code, function(err) {
-        if (err) throw err;
-        parseFile(TMP_GO_FILE_PATH, function(ast){
-            if (!ast){
-                throw new Error("No ast tree was generated");
-            }else{
-                callback(ast);
-            }
-        });
+    var wrapped_code = wrap_prefix + '\nfunc main(){\n' + code + '}';
+    _parseCode(wrapped_code, function(ast) {
+        if (!ast) {
+            callback(new Error("No ast tree was generated"));
+        } else {
+            callback(ast);
+        }
     });
 }
 
 function deleteFiles(filename) {
     var jsfile = filename + ".js";
-    var beamfile = filename + ".beam";
+    var gofile = filename + ".go";
 
-    if (fs.existsSync(jsfile))
-        fs.unlinkSync(jsfile);
-    
-    if (fs.existsSync(beamfile))
-        fs.unlinkSync(beamfile);
-
-    if (fs.existsSync(TMP_GO_FILE_PATH))
-        fs.unlinkSync(TMP_GO_FILE_PATH);
+    fs.unlink(jsfile, function(err) {});
+    fs.unlink(gofile, function(err) {});
 }
 
 //Handle user input
@@ -88,7 +75,7 @@ program
     .option('-o, --output <file>', 'Output to js file', outputFile)
     .parse(process.argv);
 
-if (program.args[0]!="test/test.js"){
+if (program.args[0] != "test/test.js") {
     if (program.args.length > 0) {
         INPUT_source_file_path = program.args[0];
     } else {
@@ -100,61 +87,105 @@ function cleanUp(code, filename) {
     deleteFiles(filename);
 }
 
-if (INPUT_source_file_path){
+if (INPUT_source_file_path) {
     parseFile(INPUT_source_file_path);
 }
 
-function parseFile(source_file_path, callback) {
-    if (fs.existsSync(source_file_path)) {
 
-        exec(['./deps/go2js', source_file_path], function(err, out, code) {
-            if (err instanceof Error)
-                throw err;
-            process.stderr.write(err);
-
-            var source_full_fileName = path.basename(source_file_path);
-            var filename = source_full_fileName.split(".")[0];
-            var js_filename = filename + ".js";
-            var output_js = DEFAULT_JS_OUTPUT_DIR_PATH + js_filename
-            
-            fs.writeFile(output_js, out, function(err) {
-                if (err) {
-                    throw err;
-                }
-                var jsCode = fs.readFileSync(output_js, "utf8");
-                var ast = esprima.parse(jsCode, {
-                    loc: true
-                });
-
-                if (callback){
-                    callback(ast);
-                    cleanUp(code, filename);
-                }else{
-                    if (JS_AST_OUTPUT_ENABLED) {
-                        process.stdout.write(JSON.stringify(ast, null, 4));
-                    }
-
-                    if (JS_OUTPUT_FILE) {
-                        mv(output_js, JS_OUTPUT_FILE, {
-                            mkdirp: true
-                        }, function(err) {
-                            if (err instanceof Error)
-                                throw err;
-                            cleanUp(code, filename);
-                        });
+function _parseCode(code, callback) {
+    try {
+        tmp.file({
+            mode: 0644,
+            postfix: '.go'
+        }, function _tempFileCreated(err, path, fd) {
+            if (err) throw err;
+            fs.writeFile(path, code, function(err) {
+                if (err) throw err;
+                parseFile(path, function(ast) {
+                    if (!ast) {
+                        callback(new Error("No ast tree was generated"));
                     } else {
-                        cleanUp(code, filename);
+                        callback(ast);
                     }
-                }
+                });
             });
         });
-    } else {
-        console.log("golang file path is invalid");
+    } catch (err) {
+        console.error(err);
     }
 }
 
+function parseFile(source_file_path, callback) {
+    fs.exists(source_file_path, function(exists) {
+        if (exists) {
+            exec(['./deps/go2js', source_file_path], function(err, out, code) {
+                if (err instanceof Error) {
+                    if (callback) {
+                        callback(err);
+                    } else {
+                        console.error(err);
+                    }
+                } else {
+                    var source_full_fileName = path.basename(source_file_path);
+                    var filename = source_full_fileName.split(".")[0];
+                    var js_filename = filename + ".js";
+                    var output_js = DEFAULT_JS_OUTPUT_DIR_PATH + js_filename
 
-module.exports={
+                    fs.writeFile(output_js, out, function(err) {
+                        if (err) {
+                            if (callback) {
+                                callback(err);
+                            } else {
+                                console.error(err);
+                            }
+                        } else {
+
+                            fs.readFile(output_js, function(err, data) {
+                                if (err) {
+                                    if (callback) {
+                                        callback(err);
+                                    } else {
+                                        console.error(err);
+                                    }
+                                }
+                                var jsCode = data;
+                                var ast = esprima.parse(jsCode, {
+                                    loc: true
+                                });
+
+                                if (callback) {
+                                    callback(ast);
+                                    cleanUp(code, filename);
+                                } else {
+                                    if (JS_AST_OUTPUT_ENABLED) {
+                                        console.log(JSON.stringify(ast, null, 4));
+                                    }
+
+                                    if (JS_OUTPUT_FILE) {
+                                        mv(output_js, JS_OUTPUT_FILE, {
+                                            mkdirp: true
+                                        }, function(err) {
+                                            if (err instanceof Error)
+                                                throw err;
+                                            cleanUp(code, filename);
+                                        });
+                                    } else {
+                                        cleanUp(code, filename);
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        } else {
+            console.log("golang file path is invalid");
+        }
+    });
+}
+
+
+module.exports = {
     parse: parse,
     wrap: wrap
 }
